d4550d97af87cbfd0450806345ec47a8966c7b4f
minglestorne <marcinglesfullstack@gmail.com>
2025-07-07 15:36:51 +0000

    Dominios Completed

---
+++ app.js
@@ -0,0 +1,18 @@
+// Nombres
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extensiones = ['.com','.net']
+
+// Loop dominios
+  for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {;
+  for (let k = 0; k < noun.length; k++) {;
+  for (let l = 0; l < extensiones.length; l++) {;
+  console.log(pronoun[i]+adj[j]+noun[k]+extensiones[l]);
+  }
+ }
+}
+}
+
+  
