99a842a00269b3fd1f8ffe2f4fdab2065c92d3b4
minglestorne <marcinglesfullstack@gmail.com>
2025-07-08 21:38:36 +0000

    Lineamiento

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
   let extensiones = ['.com','.net']
 
 // Loop dominios
-  for (let i = 0; i < pronoun.length; i++) {
+for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {;
   for (let k = 0; k < noun.length; k++) {;
   for (let l = 0; l < extensiones.length; l++) {;
